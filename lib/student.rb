ee418395807db502dbfcb9f322eaf53f7a7e74f8
Shea Kusiak <shkusiak@gmail.com>
2018-04-17 01:30:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,6 @@
 class Student
   def hello
-    print "Hey there!"
-    print " I'm so excited to learn stuff.\n"
+    print "Hey there! I'm so excited to learn stuff.\n"
   end
   def raise_hand
     print "Pick me!\n"
