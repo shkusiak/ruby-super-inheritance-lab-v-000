e06b11daac83154eb83cfbc5e41a1fd9ce65d2bf
Shea Kusiak <shkusiak@gmail.com>
2018-04-17 01:29:45 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,6 +1,6 @@
 class ChattyStudent < Student
   def hello
-    super + " I'm so excited to learn
+    super & " I'm so excited to learn
 stuff.\nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dea
 d last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay wel
 l let me just tell you who died...\n"
