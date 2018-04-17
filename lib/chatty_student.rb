48b39b86c668b6fb36f96f6b255a72c0a687b66a
Shea Kusiak <shkusiak@gmail.com>
2018-04-17 01:31:11 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,6 +1,6 @@
 class ChattyStudent < Student
   def hello
-    super 
+    super
     print "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dea
 d last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay wel
 l let me just tell you who died...\n"
